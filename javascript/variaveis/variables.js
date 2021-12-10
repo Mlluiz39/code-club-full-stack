75a6c9af2842143ce9c49c1f9418b1317fc176f1
Marcelo <mlluizpereira39@gmail.com>
2021-12-10 09:52:37 -0300

    variaveis

---
+++ javascript/variaveis/variables.js
@@ -0,0 +1,34 @@
+//VARIÁVEIS
+
+var number1 = 1
+let number2 = 2
+const number3 = 3
+
+number1 = 10
+number2 = 20
+const number4 = 30
+
+console.log(number1)
+console.log(number2)  
+console.log(number3)  // não pode ser alterado
+console.log(number4)
+
+// VARIÁVEIS QUE TENHA UM SIGNIFICADO MAIS ESPECÍFICO
+
+//PERMITIDO:
+
+// - Iniciar com $ ou _
+// - Colocar acentos
+// - Iniciar com letras maiúsculas ou minúsculas
+
+// NÃO PODE:
+// - Não pode começar com números
+// - Não pode conter espaços
+
+//IDEAL:
+
+// - CamelCase = meuNomeEmCamelCase
+// - PascalCase = MeuNomeEmPascalCase
+// - SnakeCase = meu_nome_em_snake_case
+// - Sempre em inglês
+// - nomes que fazem sentido para o contexto
