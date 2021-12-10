a4499a1e866f07058656285705f49c80b49c03a8
Marcelo <mlluizpereira39@gmail.com>
2021-12-10 10:02:35 -0300

    tipo de dado string

---
+++ javascript/tiposDeDados/string.js
@@ -0,0 +1,16 @@
+//STRING
+// - Cadeia de caracteres
+
+let firstStringMethod = 'Hello World 1'
+let secondStringMethod = "Hello World 2"
+let thirdStringMethod  = `Hello World 3`  /* template literals / template strings */
+let numberOfPeopleInClass = 10
+
+let myTextWithSuperPowers = `Tinham exatamente ${numberOfPeopleInClass} pessoas na aula`
+myTextWithSuperPowers = `Tinham exatamente ${10 + 20} pessoas na aula`
+
+console.log(firstStringMethod)
+console.log(secondStringMethod) 
+console.log(thirdStringMethod)
+console.log(numberOfPeopleInClass)
+console.log(myTextWithSuperPowers)
