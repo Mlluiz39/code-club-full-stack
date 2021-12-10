118afdd5d2b3132b52a5eee88089451877c6b8d6
Marcelo <mlluizpereira39@gmail.com>
2021-12-10 10:55:15 -0300

    tipo de dado booleano

---
+++ javascript/tiposDeDados/boolean.js
@@ -0,0 +1,13 @@
+/*
+BOOLEAN
+*/
+
+const boolTrue = true
+const boolFalse = false
+let myNumberOne = 10
+let myNumberTwo = 20
+let myNumberThree = '20'
+
+console.log(myNumberOne == myNumberTwo)
+console.log(myNumberTwo == myNumberThree)  // aqui o JS compara o valor e não o tipo
+console.log(myNumberTwo === myNumberThree)  // aqui o JS compara o tipo e o valor
