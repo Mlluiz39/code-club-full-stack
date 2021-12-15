bdae9876c378142d757e6c3a486411e225dc275b
Marcelo <mlluizpereira39@gmail.com>
2021-12-15 15:08:38 -0300

    operadores de atribuição

---
+++ javascript/operadores/atribuicao/index.js
@@ -0,0 +1,32 @@
+/*
+Operadores de atribuição
+= -> Atribuição
++= -> Adição
+-= -> Subtração
+/= -> Divisão
+*= -> Multiplicação
+%= -> Resto
+*/
+
+let firstNumber = 10
+
+firstNumber = firstNumber + 2
+firstNumber += 2
+
+console.log(firstNumber)
+
+firstNumber -= 2
+
+console.log(firstNumber)
+
+firstNumber /= 2
+
+console.log(firstNumber)
+
+firstNumber *= 2
+
+console.log(firstNumber)
+
+firstNumber %= 2
+
+console.log(firstNumber)
