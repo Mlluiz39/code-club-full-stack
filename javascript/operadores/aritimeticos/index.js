43998ecb30244d25386a4d74052ed1387d15cdbe
Marcelo <mlluizpereira39@gmail.com>
2021-12-15 14:57:33 -0300

    operadores aritmeticos

---
+++ javascript/operadores/aritimeticos/index.js
@@ -0,0 +1,62 @@
+/*
+Operadores Aritméticos
++ ->  Adição
+- -> Subtração
+* -> Multiplicação
+/ -> Divisão
+% -> Resto ou Modulo
+++ -> Incremento
+-- -> Decremento
+** -> Exponencial
+*/
+
+let myNumber = 10
+let myNumber1 = 10
+let result = myNumber + myNumber1
+
+console.log(result)
+
+myNumber = 12
+myNumber1 = 6
+result = myNumber - myNumber1
+
+console.log(result)
+
+myNumber = 2
+myNumber1 = 9
+result = myNumber * myNumber1
+
+console.log(result)
+
+myNumber = 40
+myNumber1 = 2
+result = myNumber / myNumber1
+
+console.log(result)
+
+myNumber = 7
+myNumber1 = 3
+result = myNumber % myNumber1
+
+console.log(result)
+
+let myNumber2 = 10
+
+console.log(myNumber2++)  // aqui primeiro ele incrementa depois mostra na tela
+
+let myNumber3 = 16
+
+console.log(++myNumber3)
+
+myNumber2 = 32
+
+console.log(myNumber2--)   // aqui primeiro ele decrementa depois mostra na tela
+
+myNumber3 = 22
+
+console.log(--myNumber3)
+
+console.log(Math.pow(2, 4))
+
+console.log(2 ** 4)
+
