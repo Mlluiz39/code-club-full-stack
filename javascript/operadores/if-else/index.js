d66f7c12f03912398cc73756879b28390cb52d9f
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-07 18:47:19 -0300

    if else / else if

--- javascript/operadores/if-else/index.js
+++ javascript/operadores/if-else/index.js
@@ -2,6 +2,7 @@
 Controle de fluxo - condicionais - IF / ELSE
 if = se
 else = se não
+else if = se não, mas se
  */
 
 const age = 11
@@ -10,4 +11,14 @@ if (age >= 18) {
   console.log('maior de idade')
 } else {
   console.log('menor de idade')
+}
+
+const temperature = 40
+
+if (temperature >= 36 && temperature <= 38) {
+  console.log('A pessoa está com a temperatura normal')
+} else if (temperature <= 35) {
+  console.log('A pessoa está com a temperatura baixa')
+} else {
+  console.log('A pessoa está com a temperatura alta')
 }
