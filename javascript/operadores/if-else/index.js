f8dff27198d0fde951131c93eb43107b19fa8d46
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-07 18:41:55 -0300

    else if

---
+++ javascript/operadores/if-else/index.js
@@ -0,0 +1,13 @@
+/*
+Controle de fluxo - condicionais - IF / ELSE
+if = se
+else = se não
+ */
+
+const age = 11
+
+if (age >= 18) {
+  console.log('maior de idade')
+} else {
+  console.log('menor de idade')
+}
