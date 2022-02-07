ecf3f82c20f58653e5c1df242be0371bc3751e57
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-07 18:18:05 -0300

    operadores logicos

---
+++ javascript/operadores/oper-logicos/index.js
@@ -0,0 +1,23 @@
+const log = (...value) => console.log(value);
+
+/*
+Operadores Lógicos
+
+&& -> E -> AND  // FILHO OBEDIENTE
+true && true -> true
+true && false -> false
+false && true -> false
+false && false -> false
+
+|| -> OU -> OR  // FILHO ESPERTINHO
+true || true -> true
+true || false -> true
+false || true -> true
+false || false -> false
+
+! -> NÃO -> NOT  // FILHO DO CONTRA / REBELDE
+!true -> false
+!false -> true
+*/
+
+log(!true)
