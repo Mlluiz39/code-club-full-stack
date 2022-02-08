bb6d6949c64dcb53184ba5acd86460dc4265f959
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-08 17:32:45 -0300

    desafio 02 js

--- projects/desafio-2-js/app.js
+++ projects/desafio-2-js/app.js
@@ -27,11 +27,10 @@ Ao rodar o programa, deve imprimir se a pessoa é aprovada ou não no processo ,
 [ ] Faça um programa onde ele leia 4 números. Em cada caso, ele deve retornar a mensagem condizente: - Todos os números são impares => TODOS OS NÚMEROS SÃO IMPARES - Todos os números são pares => TODOS OS NÚMEROS SÃO PARES. Em caso negativo dos casos a cima, colocar uma mensagem => Há números pares e ímpares.
 */
 
-const log = (msg) => console.log(msg);
+const log = msg => console.log(msg)
 
 const myNumber = 10
 let valueClient = 5
-
 if (valueClient > myNumber) {
   log('O número é maior')
 } else if (valueClient < myNumber) {
@@ -51,17 +50,123 @@ log(client['nationality'] === 'argentino')
 
 const myLuckyNumber = 5
 const numberRandom = Math.floor(Math.random() * 10 + 1)
-
-numberRandom == myLuckyNumber ? log('esse número é o número ganhador parabéns!') : log('esse não é o numero sorteado tente novamente!')
+numberRandom == myLuckyNumber
+  ? log('esse número é o número ganhador parabéns!')
+  : log('esse não é o numero sorteado tente novamente!')
 
 const myNumber1 = 17
 const myNumber2 = 26
 const myNumber3 = 19
-
 if (myNumber1 > myNumber2 && myNumber1 > myNumber3) {
   log('O número 1 é o maior')
-}else if (myNumber2 > myNumber1 && myNumber2 > myNumber3) {
+} else if (myNumber2 > myNumber1 && myNumber2 > myNumber3) {
   log('O número 2 é o maior')
 } else {
-  log('O número 3 é o maior') 
+  log('O número 3 é o maior')
+}
+
+const customer = [
+  (customerOne = {
+    name: 'Marcelo',
+    age: '42',
+    sex: 'masculino',
+    profession: 'programador',
+    nationality: 'brasileira',
+  }),
+  (customerTwo = {
+    name: 'Julia',
+    age: '17',
+    sex: 'feminino',
+    profession: 'estudante',
+    nationality: 'brasileira',
+  }),
+  (customerThree = {
+    name: 'Kaique',
+    age: '20',
+    sex: 'masculino',
+    profession: 'mecânico de motos',
+    nationality: 'brasileira',
+  }),
+  (customerFour = {
+    name: 'Elaine',
+    age: '38',
+    sex: 'Feminino',
+    profession: 'Técnico segurança trabalho',
+    nationality: 'brasileira',
+  }),
+  (customerFive = {
+    name: 'Matheus',
+    age: '15',
+    sex: 'masculino',
+    profession: 'estudante',
+    nationality: 'brasileira',
+  }),
+]
+if (customer[4].age >= 18 && customer[4].nationality === 'brasileira') {
+  log('pessoa aprovada no processo!')
+} else {
+  log('pessoa não aprovada no processo!')
+}
+
+const note = 5
+switch (note) {
+  case 1:
+    log('nota muito baixa')
+    break
+  case 2:
+    log('nota baixa')
+    break
+  case 3:
+    log('nota media')
+    break
+  case 4:
+    log('nota boa')
+    break
+  case 5:
+    log('nota muito boa')
+    break
+  case 6:
+    log('nota excelente')
+    break
+  case 7:
+    log('nota mais que excelente')
+    break
+  case 8:
+    log('nota ótima')
+    break
+  case 9:
+    log('nota mais que ótima')
+    break
+  case 10:
+    log('nota excepcional parabéns!')
+    break
+  default:
+    log('nota inválida')
+    break
+}
+
+const evenOrOddNumber = 4
+evenOrOddNumber % 2 === 0
+  ? log('esse número é par')
+  : log('esse número é impar')
+
+const primeAndImparNumber = 7
+if (primeAndImparNumber % 2 === 0) {
+  log('esse número é par')
+} else if (primeAndImparNumber % 5 === 0) {
+  log('esse número é divisível por 5')
+} else if (primeAndImparNumber % 3 === 0) {
+  log('esse número é impar')
+} else if (primeAndImparNumber % 7 === 0) {
+  log('esse número é primo')
+}
+
+const oddAndEvenNumbers = [2, 47, 6, 29]
+for (let i = 0; i < oddAndEvenNumbers.length; i++) {
+  const number = oddAndEvenNumbers[i]
+  if (number % 2 === 0) {
+    log('o numero ' + [number] + ' é par')
+  } else {
+    log('o número ' + [number] + ' é impar')
+  }
 }
