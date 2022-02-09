01dcab8392e9d052326997c81ab9668ef00ce985
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-09 19:29:11 -0300

    function arguments and parameter

---
+++ javascript/funcoes/function-args-parameter/index.js
@@ -0,0 +1,11 @@
+/*
+Functions
+Arguments and Parameters
+*/
+
+function sum(number1, number2) {
+  console.log(number1 + number2)
+}
+
+sum(2, 3) // Invoca a Função / Invoke or call the function
+sum(5, 8)
