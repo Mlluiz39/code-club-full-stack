4f05541df7d5d9824fa4f48ef38926f7e2395bad
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-09 19:23:41 -0300

    primeira function

---
+++ javascript/funcoes/function/index.js
@@ -0,0 +1,12 @@
+/*
+Funções / Functions
+*/
+
+console.log('Code Club é uma comunidade de programadores')
+console.log('Code Club é uma comunidade de programadores')
+
+function printingAboutCodeClub() {
+  console.log('Code Club é uma comunidade de programadores que se reúne para aprender e compartilhar conhecimento.')
+}
+
+printingAboutCodeClub()
