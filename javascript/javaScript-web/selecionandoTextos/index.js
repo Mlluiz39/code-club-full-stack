a8ea78fce2d5fde64c839c815cc0821f228fd6db
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-21 19:18:05 -0300

    manipulando textos

---
+++ javascript/javaScript-web/selecionandoTextos/index.js
@@ -0,0 +1,9 @@
+const element = document.querySelector('h1')
+const element2 = document.querySelector('h2')
+const element3 = document.querySelector('h3')
+
+element.innerText = 'Hello World 1'
+
+element2.textContent = 'Hello World 2'
+
+element3.innerHTML = 'Hello World 3 <i>My paragraph</i>'
