76227edc9eb104b3ec57efe9481e7e5a0a3a77ee
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-21 19:08:48 -0300

    selecionando atributos

---
+++ javascript/javaScript-web/selecionandoAtributos/index.js
@@ -0,0 +1,15 @@
+/*
+Selecionando Atributos:
+
+
+
+*/
+
+const log = (msg) => console.log(msg)
+
+const element = document.querySelector('input')
+const image = document.querySelector('img')
+
+log(element.value)
+
+log(image.src)
