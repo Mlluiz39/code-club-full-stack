86fd2acddbb058e8b3cb8405874090b825597153
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-21 19:26:56 -0300

    modificando estilos

---
+++ javascript/javaScript-web/manipulandoEstilos/index.js
@@ -0,0 +1,11 @@
+const element = document.querySelector('h1')
+const bodyElement = document.querySelector('body')
+
+element.style.color = 'white'
+element.style.fontSize = '50px'
+element.style.backgroundColor = '#746767'
+element.style.textAlign = 'center'
+element.style.padding = '20px'
+element.style.margin = '55px 95px'
+
+bodyElement.style.backgroundColor = '#ca9f6f'
