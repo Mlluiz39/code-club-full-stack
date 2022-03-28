bfcbcf4aadb1e9ccbc07a7bace55665c173ce7df
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-03-28 13:16:19 -0300

    add to promises

---
+++ javascript/promises/promises.js
@@ -0,0 +1,56 @@
+const rand = (min, max) => {
+  min *= 1000
+  max *= 1000
+  const randomCalc = Math.floor(Math.random() * (max - min + 1)) + min
+  return randomCalc
+}
+
+const holdOn = (msg, time) => {
+  setTimeout(() => {
+    console.log(msg)
+  }, time)
+}
+
+holdOn('Hold 1', rand(1, 3))
+holdOn('Hold 2', rand(1, 3))
+holdOn('Hold 3', rand(1, 3))
+
+// como ficaria com callback
+const randT = (min, max) => {
+  min *= 1000
+  max *= 1000
+  const randomCalc = Math.floor(Math.random() * (max - min + 1)) + min
+  return randomCalc
+}
+
+const holdOnT = (msg, time, callback) => {
+  setTimeout(() => {
+    console.log(msg)
+    if (callback) callback()
+  }, time)
+}
+
+holdOnT('callback 1', rand(1, 3), () => {
+  holdOnT('callback 2', rand(1, 3), () => {
+    holdOnT('callback 3', rand(1, 3))
+  })
+})
+
+// Trabalhando com PROMISES
+const holdOnTree = (msg, time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(msg)
+    }, time)
+  })
+}
+
+holdOnTree('Conexão com DB', rand(1, 3))
+.then((msg) => {console.log(msg) 
+  return holdOnTree('Buscando dados na base', rand(1, 4))})
+  .then((msg) => {console.log(msg)
+    return holdOnTree('Tratando os dados da base', rand(1, 5))})
+    .then((msg) => {console.log(msg)  
+      return holdOnTree('Retornando os dados da base !', rand(1, 6))})
+      .then((msg) => {console.log(msg)})
+        .then(() => {console.log('Fim da conexão com DB')})
