b4a48ea26d2da38b2a5320034ab8af4f6d501235
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-03-29 21:58:40 -0300

    body params

--- nodeJS/first-project-node/index.js
+++ nodeJS/first-project-node/index.js
@@ -1,11 +1,14 @@
 const { request } = require('express')
 const express = require('express')
-const app = express()
+
 const port = 3000
+const app = express()
+app.use(express.json())
 
 /*
 - Query params => ?nome=valor&idade=valor  => req.query (objeto) Filtros de busca
-- Route params => /users/:id (users/1) => req.params BUSCAR, DELETAR, ATUALIZAR 
+- Route params => /users/:id (users/1) => req.params BUSCAR, DELETAR, ATUALIZAR
+- Request Body => { "name": "Marcelo", "email": "mlluiz@gmail.com" } => req.body (objeto)
 */
 
 app.get('/users', (req, res) => {
@@ -19,6 +22,13 @@ app.get('/users/:id', (req, res) => {
   const identificador = req.params.id
   const { id } = req.params
   return res.json({ id, identificador })
+})
+
+app.get('/user', (req, res) => {
+  const { name, age } = req.body
+  console.log(req.body)
+
+  return res.json({ name, age })
 })
 
 app.listen(port, () => console.log(`🚀 Servidor rodando na porta: ${port}!`))
