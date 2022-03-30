58b96b6c3d9d81ce2d36f3744593564aa43f98bc
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-03-29 21:37:58 -0300

    usando query params

--- nodeJS/first-project-node/index.js
+++ nodeJS/first-project-node/index.js
@@ -1,7 +1,18 @@
+const { request } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/users', (request, response) => response.send('Hello World!'))
+/*
+- Query params => ?nome=valor&idade=valor  => req.query (objeto) Filtros de busca
+- Route params => /users/:id (users/1) => req.params BUSCAR, DELETAR, ATUALIZAR 
+*/
+
+app.get('/users', (request, response) => {
+  const name = request.query.name // podemos usar destructuring para pegar o nome e a idade
+  const age = request.query.age
+  const { nome, idade } = request.query
+  return response.json({ name, age, nome, idade })
+})
 
 app.listen(port, () => console.log(`🚀 Servidor rodando na porta: ${port}!`))
