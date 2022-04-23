3316f92543b632651b066ac46cfca3c8237d0fa9
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-04-23 12:59:25 -0300

    classes e objetos

--- javascript/javaScript-avancado/classes/index.js
+++ javascript/javaScript-avancado/classes/index.js
@@ -28,7 +28,7 @@ class Person1 {
   constructor(name, age) {
     console.log(`Hello, I'm ${name}`)
 
-    this.name = name  
+    this.name = name
     this.age = age
     /* this é utilizado para expor a variável para o escopo global da 
        função construtora
@@ -49,3 +49,35 @@ newPersonJuuh.talk()
 
 const newPersonLuiz = new Person1('Marcelo Luiz', 42)
 newPersonLuiz.talk()
+
+/**
+ * Criando objetos caso seja poucos, usamos spreed
+ */
+
+const user = {
+  name: 'Marcelo',
+  email: 'mlluiz@gmail.com',
+  login: () => 'O usuário logou',
+  logout: () => 'O usuário deslogou',
+}
+
+const user0 = {
+  ...user,
+  name: 'Julia Souza',
+  email: 'Julia@gmail.com',
+}
+
+console.log(user, user0)
+
+class User {
+  constructor(name, email) {
+    this.name = name
+    this.email = email
+  }
+}
+
+const user1 = new User('Marcelo Luiz', 'mlluiz@gmail.com')
+const user2 = new User('Julia Souza', 'juuh@gmail.com')
+
+
+console.log(user1, user2)
