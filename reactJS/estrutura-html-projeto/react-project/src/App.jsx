9503d80d78a957d6b8b2c6b2bd0abac8aa118f4d
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-06-07 21:29:47 -0300

    add to logical components

--- reactJS/estrutura-html-projeto/react-project/src/App.jsx
+++ reactJS/estrutura-html-projeto/react-project/src/App.jsx
@@ -1,22 +1,52 @@
 import * as S from './styles'
 
-function App() {
+import Peoples from './assets/peoples.svg'
+import Arrow from './assets/arrow.svg'
+import Trash from './assets/trash.svg'
+
+export default function App() {
+  const users = [
+    {
+      id: Math.round(),
+      name: 'Marcelo Luiz',
+      email: 'mlluiz@gmail.com',
+    },
+    {
+      id: Math.round(),
+      name: 'Julia Souza',
+      email: 'juuh@gmail.com',
+    },
+  ]
   return (
     <S.Container>
-      <S.Image />
+      <S.Image src={Peoples} alt="logo imagem" />
       <S.ContainerItems>
         <S.H1>Olá!</S.H1>
 
         <S.InputLabel>Nome</S.InputLabel>
         <S.Input placeholder="Nome" />
 
-        <S.InputLabel>Idade</S.InputLabel>
-        <S.Input placeholder="Idade" />
+        <S.InputLabel>E-mail</S.InputLabel>
+        <S.Input placeholder="e-mail" />
 
-        <S.Button>Cadastrar</S.Button>
+        <S.Button>
+          Cadastrar
+          <img src={Arrow} alt="imagem de seta" />
+        </S.Button>
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>
+              <S.User>
+                <S.UserName>{user.name} </S.UserName>
+                <S.UserEmail>{user.email}</S.UserEmail>
+                <button>
+                  <img src={Trash} alt="image trash" />
+                </button>
+              </S.User>
+            </li>
+          ))}
+        </ul>
       </S.ContainerItems>
     </S.Container>
   )
 }
-
-export default App
