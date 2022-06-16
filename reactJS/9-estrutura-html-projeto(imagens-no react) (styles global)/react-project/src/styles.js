9a27f6b5e25d51ec23907f77041f944b60e92a6a
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-06-16 12:26:51 -0300

    remove paste global style

---
+++ reactJS/9-estrutura-html-projeto(imagens-no react) (styles global)/react-project/src/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import Background from '../src/assets/bg-image-1.svg'
+
+export const Container = styled.main`
+  background: url(${Background});
+  background-size: cover;
+`
+
+export const Image = styled.img`
+`
+
+export const ContainerItems = styled.section`
+`
+
+export const H1 = styled.h1`
+`
+
+export const InputLabel = styled.p`
+  
+`
+
+export const Input = styled.input`
+  
+`
+
+export const Button = styled.button`
+ 
+`
